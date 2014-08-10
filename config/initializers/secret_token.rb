4182a64bc222e11710a1d6dbce39152bc6060feb
sunkencity999 <theroyalart@gmail.com>
2014-08-10 12:13:13 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Politiwiki::Application.config.secret_key_base = '5ad64d35430737cf180c7094bbe230e3a3e6854589cf32de78369ad1bec13bfb24652d62dafb857fcbd61c1b553e84ad547583165c279839d10e1f401e298c06'
