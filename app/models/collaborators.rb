bc7171112e360f6bfaa738fd0d8c29e55980abd7
sunkencity999 <theroyalart@gmail.com>
2014-08-11 23:21:14 -0700

    setup HABTM relationships for users, wiki, collaborators

--- app/models/collaborators.rb
+++ app/models/collaborators.rb
@@ -1,2 +1,4 @@
 class Collaborators < ActiveRecord::Base
+  has_and_belongs_to_many :wikis, through: :users
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+    has_and_belongs_to_many :wikis
+   has_and_belongs_to_many :collaborators
+
+
 end
